0f6e72f9c3ef62663a654c6cc0290996f587822a
= <git@ganey.co.uk>
2015-10-20 15:00:12 +0100

    Updated script to set status better

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e -x
 echo "setting status"
-cp -R /usr/share/www/_ah /var/www/public/_ah
+mkdir /var/www/public/_ah
+cp -r /usr/share/www/_ah/* /var/www/public/_ah/
 echo "starting nginx"
 service nginx restart
 /usr/bin/memcached -d -s /var/run/memcached.sock -m 64 -c 1024 -u root
